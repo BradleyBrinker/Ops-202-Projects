bd1a073f14559029af9ba753f38209dbec6a2d37
BradleyBrinker <bradleybrinker@gmail.com>
2023-04-27 01:35:53 +0000

    OpsChallenge3

---
+++ bash/OpsChal3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+function login () {
+    last
+}
+login
+echo "This is my login history"
+#end
---
+++ bash/helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Bradley Brinker
+# Date of latest revision:      4/25/2023
+# Purpose:                      Print Hello World on Screen
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
