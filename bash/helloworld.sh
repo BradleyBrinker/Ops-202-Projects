07312744bcb5d68953c10dfce25546d60080483e
BradleyBrinker <bradleybrinker@gmail.com>
2023-04-27 01:50:54 +0000

    hellworld opschal

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
+#!/bin/bash 
+echo Hello World
+#end
 
 # Script Name:                  Hello World
 # Author:                       Bradley Brinker
 # Date of latest revision:      4/25/2023
+
 # Purpose:                      Print Hello World on Screen
-
-# Declaration of variables
-
-# Declaration of functions
-
-
-# Main
-
-
-
-# End
