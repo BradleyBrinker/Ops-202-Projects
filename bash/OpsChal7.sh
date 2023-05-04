49ef56bf8b8a8ed3d507c460e6ddaa34727c56c5
BradleyBrinker <bradleybrinker@gmail.com>
2023-05-04 19:46:07 -0400

    opschallenge7

---
+++ bash/OpsChal7.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "=====SYSTEM INFORMATION====="
+
+echo "-CPU-"
+sudo lshw -c cpu| grep -i "Product"
+sudo lshw -c cpu | grep -i "Vendor"
+sudo lshw -c cpu | grep -i "Physical ID"
+sudo lshw -c cpu | grep -i "Bus info"
+sudo lshw -c cpu | grep -i "Width"
+
+echo "-RAM-"
+sudo lshw -c memory | grep -i "Description"
+sudo lshw -c memory | grep -i "Physical ID"
+sudo lshw -c memory | grep -i "Size"
+
+echo "-Display Adapter"
+sudo lshw -c display | grep -i "Description"
+sudo lshw -c display | grep -i "Product"
+sudo lshw -c display | grep -i "Vendor"
+sudo lshw -c display | grep -i "Physical ID"
+sudo lshw -c display | grep -i "Bus Info"
+sudo lshw -c display | grep -i "Width"
+sudo lshw -c display | grep -i "Clock"
+sudo lshw -c display | grep -i "Capabilities"
+sudo lshw -c display | grep -i "Configuration"
+sudo lshw -c display | grep -i "Resources"
+
+echo "Network Adapter"
+sudo lshw -c network | grep -i "Description"
+sudo lshw -c network | grep -i "Product"
+sudo lshw -c network | grep -i "Vendor"
+sudo lshw -c network | grep -i "Physical ID"
+sudo lshw -c network | grep -i "Bus Info"
+sudo lshw -c network | grep -i "Logical name"
+sudo lshw -c network | grep -i "Version"
+sudo lshw -c network | grep -i "Serial"
+sudo lshw -c network | grep -i "Size"
+sudo lshw -c network | grep -i "Width"
+sudo lshw -c network | grep -i "Clock"
+sudo lshw -c network | grep -i "Capabilities"
+sudo lshw -c network | grep -i "Configuration"
+sudo lshw -c network | grep -i "Resources"
+
+#end
